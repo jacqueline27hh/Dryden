6d5426ef92b2710c56d27083f73174ce3b1c5271
Jackie <jacqueline27hh@gmail.com>
2014-02-08 19:25:59 -0500

    jackie - html + css template

---
+++ website/html/js/script.js
@@ -0,0 +1,41 @@
+/*
+
+My Custom JS
+============
+
+Author:  Brad Hussey
+Updated: August 2013
+Notes:	 Hand coded for Udemy.com
+
+*/
+
+$(function() {
+	$('#alertMe').click(function(e) {
+		e.preventDefault(); //e -> prevent going to the link or refresh the page because "alertMe" is a link
+
+		$('#successAlert').slideDown();  //slide down success alert
+	});
+
+	$('a.pop').click(function(e){
+		e.preventDefault();
+
+	$('a.pop').popover();
+
+	$('[rel="tooltip"]').tooltip();
+
+
+
+	});
+
+<script type="text/javascript">
+  $(document).ready(function() {
+    $('.carousel').carousel({
+      interval: 1200
+    })
+  });
+</script>
+
+
+
+});
+
