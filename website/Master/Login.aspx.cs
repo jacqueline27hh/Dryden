b17bafce72b8cf8032b3d034e2e4b778d745a77a
kategol <kate.gollogly12@gmail.com>
2014-02-21 13:17:18 -0500

    Kate - updated login

--- website/Master/Login.aspx.cs
+++ website/Master/Login.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Web.Security;
 
 public partial class Login : System.Web.UI.Page
 {
@@ -14,29 +15,29 @@ public partial class Login : System.Web.UI.Page
 
     protected void main_login_Authenticate(object sender, AuthenticateEventArgs e)
     {
-        if (Membership.ValidateUser(main_login.UserName, main_login.Password))
-        {
+    //    if (Membership.ValidateUser(main_login.UserName, main_login.Password))
+    //    {
 
-            if (Roles.IsUserInRole(main_login.UserName, "Admin"))
-            {
-                main_login.DestinationPageUrl = "~/Admin-Dashboard/Admin.aspx";
-            }
-            else if (Roles.IsUserInRole(main_login.UserName, "Doctor"))
-            {
-                main_login.DestinationPageUrl = "~/Doctor-Dashboard/Doctor.aspx";
-            }
-            else if (Roles.IsUserInRole(main_login.UserName, "Patient"))
-            {
-                main_login.DestinationPageUrl = "~/Patient-Dashboard/Patient.aspx";
-            }
-            else
-            {
-                Response.Write("Sorry, an error occured");
-            }
-        }
-        else
-        {
-            Response.Write("Invalid login");
-        }
+    //        if (Roles.IsUserInRole(main_login.UserName, "Admin"))
+    //        {
+    //            main_login.DestinationPageUrl = "~/Admin-Dashboard/Admin.aspx";
+    //        }
+    //        else if (Roles.IsUserInRole(main_login.UserName, "Doctor"))
+    //        {
+    //            main_login.DestinationPageUrl = "~/Doctor-Dashboard/Doctor.aspx";
+    //        }
+    //        else if (Roles.IsUserInRole(main_login.UserName, "Patient"))
+    //        {
+    //            main_login.DestinationPageUrl = "~/Patient-Dashboard/Patient.aspx";
+    //        }
+    //        else
+    //        {
+    //            Response.Write("Sorry, an error occured");
+    //        }
+    //    }
+    //    else
+    //    {
+    //        Response.Write("Invalid login");
+    //    }
     }
 }
