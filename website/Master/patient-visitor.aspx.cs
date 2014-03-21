c14730a9053a0cfaac2e3e7fde81517a61fcb258
justinog <jogdev77@gmail.com>
2014-03-20 22:04:38 -0400

    Justin - more pf changes

--- website/Master/patient-visitor.aspx.cs
+++ website/Master/patient-visitor.aspx.cs
@@ -7,21 +7,27 @@ using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
+    surveyClass objSurvey = new surveyClass();
     protected void Page_Load(object sender, EventArgs e)
     {
 
         string urlPost = HttpContext.Current.Request.Url.AbsoluteUri;
-        string[] indexNum  = urlPost.Split('=');
+        string[] indexNum = urlPost.Split('=');
         if (indexNum.Length > 1)
         {
-           mv_tabs.ActiveViewIndex = Convert.ToInt32(indexNum[1]);
-           pv_nav_menu.FindItem(indexNum[1]).Selected = true;  
+            mv_tabs.ActiveViewIndex = Convert.ToInt32(indexNum[1]);
+            pv_nav_menu.FindItem(indexNum[1]).Selected = true;
         }
-        
+
     }
 
     protected void switchTabs(object sender, EventArgs e)
     {
         mv_tabs.ActiveViewIndex = Convert.ToInt32(pv_nav_menu.SelectedValue);
     }
+    protected void subSubmit(object sender, EventArgs e)
+    {
+        
+    }
 }
+    
