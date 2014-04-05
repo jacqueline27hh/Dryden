1eb67b84b72357e0a240410925377b0799f71d90
Michaeljdu <michaeljdu2010@gmail.com>
2014-04-04 17:59:04 -0700

    Mike_ because github is making me

--- website/Master/Admin-Dashboard/search.aspx.cs
+++ website/Master/Admin-Dashboard/search.aspx.cs
@@ -7,5 +7,35 @@ using System.Web.UI.WebControls;
 
 public partial class admin_search : System.Web.UI.Page
 {
-    
+
+    public void subCommand(object sender, RepeaterCommandEventArgs e)
+    {
+        switch (e.CommandName)
+        {
+            case "sub_update":
+                searchClass searchInstance = new searchClass();
+                TextBox txtfname = (TextBox)e.Item.FindControl("txt_fname");
+                TextBox txtlname = (TextBox)e.Item.FindControl("txt_lname");
+                TextBox txtdepartment = (TextBox)e.Item.FindControl("txt_department");
+                TextBox txtphone = (TextBox)e.Item.FindControl("txt_phone");
+                TextBox txtemail = (TextBox)e.Item.FindControl("txt_email");
+                lbl_results.Text = searchInstance.updateSearch(txtfname.Text.ToString(), txtlname.Text.ToString(),
+                    txtdepartment.Text.ToString(), txtphone.Text.ToString(), txtemail.Text.ToString(), Int32.Parse(e.CommandArgument.ToString())); 
+                break; 
+            case "sub_delete":
+                searchClass searchInstanceDel = new searchClass();
+                lbl_results.Text = searchInstanceDel.deleteSearch(Int32.Parse(e.CommandArgument.ToString())); 
+                break;
+            case "sub_insert":
+                searchClass searchInstanceInsert = new searchClass();
+                TextBox txtfnameI = (TextBox)e.Item.FindControl("txt_fnameI");
+                TextBox txtlnameI = (TextBox)e.Item.FindControl("txt_lnameI");
+                TextBox txtdepartmentI = (TextBox)e.Item.FindControl("txt_departmentI");
+                TextBox txtphoneI = (TextBox)e.Item.FindControl("txt_phoneI");
+                TextBox txtemailI = (TextBox)e.Item.FindControl("txt_emailI");
+                lbl_results.Text = searchInstanceInsert.insertSearch(txtfnameI.Text.ToString(), txtlnameI.Text.ToString(), txtdepartmentI.Text.ToString(),
+                    txtphoneI.Text.ToString(), txtemailI.Text.ToString()); 
+                break; 
+        }
+    }
 }
---
+++ website/Master/App_Code/searchClass.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for searchClass
+/// </summary>
+public class searchClass
+{
+    public string updateSearch(string fname, string lname, string department, string phone, string email, int person_id)
+    {
+        searchClassDataContext objSearchDC = new searchClassDataContext(); 
+        using (objSearchDC)
+        {
+            var objSearchInstance = objSearchDC.search_APIs.Single(x => x.Id == person_id);
+            objSearchInstance.fname = fname; 
+            objSearchInstance.lname = lname;
+            objSearchInstance.department = department;
+            objSearchInstance.phone = phone; 
+            objSearchInstance.email = email;
+            objSearchDC.SubmitChanges(); 
+            return "Your update was successful";
+        }
+    }
+
+    public string deleteSearch(int person_id)
+    {
+        searchClassDataContext objSearchDC = new searchClassDataContext();
+        using (objSearchDC)
+        {
+            var objSearchDel = objSearchDC.search_APIs.Single(x => x.Id == person_id);
+            objSearchDC.search_APIs.DeleteOnSubmit(objSearchDel);
+            objSearchDC.SubmitChanges();
+            return "Your delete was successful";
+        }
+    }
+
+    public string insertSearch(string fname, string lname, string department, string phone, string email)
+    {
+        searchClassDataContext objSearchDC = new searchClassDataContext();
+        using (objSearchDC)
+        {
+            search_API objSearchInsert = new search_API(); 
+            objSearchInsert.fname = fname;
+            objSearchInsert.lname = lname;
+            objSearchInsert.department = department;
+            objSearchInsert.phone = phone;
+            objSearchInsert.email = email;
+
+            objSearchDC.search_APIs.InsertOnSubmit(objSearchInsert);
+            objSearchDC.SubmitChanges();
+            return "Your insert was successful"; 
+
+        }
+    }
+}
